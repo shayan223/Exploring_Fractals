653f6d564e75ce07604998c2d25598ac64413f5b
shayan223 <shayan.jalalipour@gmail.com>
2023-06-06 00:43:12 -0700

    working on window resizing

--- Complex_Number_Systems/mandelbrot_set.c
+++ Complex_Number_Systems/mandelbrot_set.c
@@ -13,10 +13,10 @@ int SWIDTH = 800;
 int SHEIGHT = 800;
 
 int ITERS = 100;
-double MIN_TEST_r = -2;
-double MAX_TEST_r = 2;
-double MIN_TEST_c = -2;
-double MAX_TEST_c = 2;
+double MIN_TEST_r = -1;
+double MAX_TEST_r = 1;
+double MIN_TEST_c = -1;
+double MAX_TEST_c = 1;
 
 void prcmx (char *control, complex c)
 //print complex number
@@ -46,6 +46,11 @@ complex sccmx ()
   return c ;
 }
 
+double parametric(double x0, double x1, double t, double power){
+    //default power should be 1
+    return x0 + pow(t,power)*(x1 - x0);
+}
+
 int check_convergence(complex z){
     if(cabs(z) > 2){//cabs greater than 2, the number diverges
         return 0;
@@ -55,19 +60,25 @@ int check_convergence(complex z){
     }
 }
 
-complex z_func(complex z, complex c){
-    return cpow(z,2) + c;
-}
 
 int test_z(double real, double complex_coef){
+    int does_converge = 0;
+    int reps = 0;//repitions to convergence
+    int i;
     complex z = 0 + 0*I;
     complex c = real + complex_coef*I;
-
-    for(int i = 0; i<ITERS; ++i){
-        z = z_func(z,c);
+    //z = cpow(z,2) + c;
+    for(i = 0; i<ITERS; ++i){
+        reps += 1;
+        z = cpow(z,2) + c;
+        
+        if(cabs(z) > 2){//check convergence
+            break;
+            //return
+        }
     }
 
-    return check_convergence(z);
+    return i;
 }
 
 int main()
@@ -76,10 +87,13 @@ int main()
 
     double converge_color [] = {102/255.0, 51/255.0, 153/255.0};
     double diverge_color [] = {34/255.0, 39/255.0, 122/255.0};
-
+    double c1 [] = {1, 0, 0};
+    double c2 [] = {0, 1, 0};
+    double c3 [] = {0, 0, 1};
 
     int key ;   
     double center[2], width[2];
+    double window_width_x, window_width_y, center_real, center_complex, width_real, width_complex ;
 
     while(key != 113){
         
@@ -87,9 +101,10 @@ int main()
 
         //one interval for a square screen
         double test_interval_x = (abs(MAX_TEST_r) + abs(MIN_TEST_r))/(double)SWIDTH;
-        double test_interval_y = (abs(MAX_TEST_c) + abs(MIN_TEST_c))/(double)SWIDTH;
+        double test_interval_y = (abs(MAX_TEST_c) + abs(MIN_TEST_c))/(double)SHEIGHT;
 
         printf("Intervals for window: x:%f   y:%f\n",test_interval_x,test_interval_y);
+        printf("Window bounds:  xmin: %f   xmax: %f    ymin: %f   ymax: %f    \n",MIN_TEST_r,MAX_TEST_r,MIN_TEST_c,MAX_TEST_c);
         double cur_real = MIN_TEST_r;
         double cur_complex = MIN_TEST_c;
         int does_converge = 0;
@@ -97,6 +112,9 @@ int main()
         G_rgb(0.3,0.3,0.3);
         G_clear();
 
+        int reps = 0;
+        double color_scale = 0;
+        double r,g,b;
         //x is along the real axis
         for(int x = 0; x < SWIDTH; ++x){
             // y is along the imaginary axis
@@ -104,15 +122,36 @@ int main()
                 //test the current complex number mapped to the current pixel coordinate
                 cur_real = MIN_TEST_r+(x*test_interval_x);
                 cur_complex = MIN_TEST_c+(y*test_interval_y);
-                does_converge = test_z(cur_real,cur_complex);
-                if(does_converge){
+                reps = test_z(cur_real,cur_complex);
+                
+                /*
+                if(reps >= ITERS){//reached max iters without converging
                     G_rgb(converge_color[0],converge_color[1],converge_color[2]);
+                    G_point(x,y);
+                }
+                else if(reps > ITERS*(2/3)){
+                    G_rgb(c1[0],c1[1],c1[2]);
+                    G_point(x,y);
+                }
+                else if(reps > ITERS*(1/3)){
+                    G_rgb(c2[0],c2[1],c2[2]);
                     G_point(x,y);
                 }
                 else{
                     G_rgb(diverge_color[0],diverge_color[1],diverge_color[2]);
                     G_point(x,y);
+                    //printf("Reps: %d, ITERS: %d", reps, ITERS);
                 }
+                */
+                //printf("Reps: %d, ITERS: %d", reps, ITERS);
+
+                color_scale = (double)reps/(double)ITERS;
+                r = parametric(converge_color[0],diverge_color[0],color_scale,3);
+                g = parametric(converge_color[1],diverge_color[1],color_scale,3);
+                b = parametric(converge_color[2],diverge_color[2],color_scale,3);
+                
+                G_rgb(r,g,b);
+                G_point(x,y);
 
             }
         }
@@ -121,16 +160,26 @@ int main()
         G_wait_click(width);
 
         //convert clicked location to complex plane
-        cur_real = MIN_TEST_r + (center[0]*test_interval_x);
-        cur_complex = MIN_TEST_c + (center[1]*test_interval_y);
+        center_real = MIN_TEST_r + (center[0]*test_interval_x);
+        center_complex = MIN_TEST_c + (center[1]*test_interval_y);
+
+        width_real = MIN_TEST_r + (width[0]*test_interval_x);
+        width_complex = MIN_TEST_c + (width[1]*test_interval_y);
 
         //compute the new starting min and max values based on the center
-        MIN_TEST_r = cur_real - width[0];
-        MAX_TEST_r = cur_real + width[0];
+        printf("CENTER REAL: %f   WIDTH REAL: %f\n", center_real,width_real);
+        window_width_x = fabs((center_real - width_real));
+        printf("WINDOW_WIDTH_x: %f \n",window_width_x);
+        window_width_y = fabs(center_complex - width_complex);
+        //convert pixel width to axis width
+        //window_width *= test_interval_x;
+        MIN_TEST_r = center_real - window_width_x;
+        MAX_TEST_r = center_real + window_width_x;
 
-        MIN_TEST_c = cur_complex - width[1];
-        MAX_TEST_c = cur_complex + width[1];
+        MIN_TEST_c = center_complex - window_width_x;
+        MAX_TEST_c = center_complex + window_width_x;
 
+        printf("WINDOW_WIDTH_x: %f \n",window_width_x);
 
 
         //key =  G_wait_key() ; // pause so user can see results
