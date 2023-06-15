3c53f75a4906bf527a8000e58590bc95b020bc72
shayan223 <shayan.jalalipour@gmail.com>
2023-06-14 19:16:49 -0700

    debugging fractal flame

--- Iterated_function_systems/fractal_flame.c
+++ Iterated_function_systems/fractal_flame.c
@@ -3,7 +3,7 @@
 
 #include  "FPToolkit.c"
 #include <math.h>
-#define HISTOGRAM_DIM 300 //MUST BE DIVISIBLE BY 3!!!
+#define HISTOGRAM_DIM 600 //MUST BE DIVISIBLE BY 3!!!
 
 /* Based on the following paper: https://flam3.com/flame_draves.pdf 
     with additional info here: https://en.wikipedia.org/wiki/Fractal_flame */
@@ -16,17 +16,17 @@ double ITERS = 6000000;
 double ERROR_TOLERANCE = 0.000001;
 double COLOR_GAMMA = 3.0; //must be greater than 1
 //viewport bounds for drawing points
-int MIN_X = -2;
-int MIN_Y = -2;
-int MAX_X = 2;
-int MAX_Y = 2;
+int MIN_X = -1;
+int MIN_Y = -1;
+int MAX_X = 1;
+int MAX_Y = 1;
 
-int SCREEN_WIDTH = 100;
-int SCREEN_HEIGHT = 100;
+int SCREEN_WIDTH = HISTOGRAM_DIM/3;
+int SCREEN_HEIGHT = HISTOGRAM_DIM/3;
 //probabilities for the respectively numbered variation functions
-double P0 = 0.1;
+double P0 = 0.2;
 double P1 = 0.2;
-double P2 = 0.3;
+double P2 = 0.2;
 double P3 = 0.2;
 double P4 = 0.2;
 
@@ -107,8 +107,8 @@ void set_color(double cur_color [], double new_color []){
     cur_color[2] = new_color[2];
 }
 
-double flame_func(double p [], double w0, double w1, double w2, double w3, double w4){
-    choice = (double)rand() / RAND_MAX ;
+void flame_func(double p [], double w0, double w1, double w2, double w3, double w4){
+
     //note, weights must add up to 1
     v0(p); p[0] *= w0; p[1] *= w0;
     v1(p); p[0] *= w1; p[1] *= w1;
@@ -121,8 +121,8 @@ int main()
 {
     int    swidth, sheight ;
     
-    int HISTOGRAM_FREQ [HISTOGRAM_DIM][HISTOGRAM_DIM] = {0};
-    double HISTOGRAM_COLOR [HISTOGRAM_DIM][HISTOGRAM_DIM][3] = {0.0};
+    static int HISTOGRAM_FREQ [HISTOGRAM_DIM][HISTOGRAM_DIM] = {0};
+    static double HISTOGRAM_COLOR [HISTOGRAM_DIM][HISTOGRAM_DIM][3] = {0.0};
     //Because doubles are not discrete, we have to divide into a grid of potential x,y values to 
     //fit everything into discrete locations in our histogram array
     double x_interval = (fabs(MAX_X - MIN_X))/(double)HISTOGRAM_DIM;
@@ -144,7 +144,7 @@ int main()
 
     //double n_iters = ITERS;
     srand(time(NULL));
-    //double choice = 0;
+    double choice = 0;
     int max_frequency = 0;
     int cur_x = 0;
     int cur_y = 0;
@@ -159,29 +159,32 @@ int main()
         choice = (double)rand() / RAND_MAX ;
         //note, this is a trick to pick each based on predifined probabilities, they MUST add up to 1
         if(choice < P0) { set_color(cur_color,dark_orange); flame_func(p, .1, .2, .3, .2, .2); }
-        if(choice < P0 + P1) { set_color(cur_color,light_blue); flame_func(p, .3, .2, .1, .2, .2); }
+        if(choice < P0 + P1) { set_color(cur_color,light_blue); flame_func(p, .1, .1, .1, .6, .1); }
         if(choice < P0 + P1 + P2) { set_color(cur_color,violet); flame_func(p, .1, .3, .1, .2, .2); }
-        if(choice < P0 + P1 + P2 + P3) { set_color(cur_color,light_green); flame_func(p, .2, .2, .2, .2, .2); }
+        if(choice < P0 + P1 + P2 + P3) { set_color(cur_color,light_green); flame_func(p, .01, .01, .09, .09, .8); }
         if(choice < P0 + P1 + P2 + P3 + P4) { set_color(cur_color,crimson); flame_func(p, .1, .6, .1, .1, .1); }
 
-        //TODO: Should I be changing/rounding p here for the next iteration??
+        //only consider points within our viewport
+        if(p[0] > MIN_X && p[0] < MAX_X && p[1] > MIN_Y && p[1] < MAX_Y){
+            //TODO: Should I be changing/rounding p here for the next iteration??
 
-        //discretize the point to match up with the histogram
-        //TODO use (int) or round() ??
-        cur_x = ((int)(((p[0] - MIN_X)/x_interval)));//round from float to nearest interval bin
-        cur_y = ((int)(((p[1] - MIN_Y)/y_interval)));
+            //discretize the point to match up with the histogram
+            //TODO use (int) or round() ??
+            cur_x = ((int)(((p[0] - MIN_X)/x_interval)));//round from float to nearest interval bin
+            cur_y = ((int)(((p[1] - MIN_Y)/y_interval)));
 
-        //Add to histogram
-        HISTOGRAM_FREQ[cur_y][cur_x] += 1;//we switch x and y because c is [row][column]
-        //update max frequency
-        if(HISTOGRAM_FREQ[cur_y][cur_x] > max_frequency){
-            max_frequency = HISTOGRAM_FREQ[cur_y][cur_x];
+            //Add to histogram
+            HISTOGRAM_FREQ[cur_y][cur_x] += 1;//we switch x and y because c is [row][column]
+            //update max frequency
+            if(HISTOGRAM_FREQ[cur_y][cur_x] > max_frequency){
+                max_frequency = HISTOGRAM_FREQ[cur_y][cur_x];
+            }
+
+            //update color for the given coordinate: the average of the newest addition and what it used to be
+            HISTOGRAM_COLOR[cur_y][cur_x][0] = (HISTOGRAM_COLOR[cur_y][cur_x][0] + cur_color[0])/2.0;
+            HISTOGRAM_COLOR[cur_y][cur_x][1] = (HISTOGRAM_COLOR[cur_y][cur_x][1] + cur_color[1])/2.0;
+            HISTOGRAM_COLOR[cur_y][cur_x][2] = (HISTOGRAM_COLOR[cur_y][cur_x][2] + cur_color[2])/2.0;
         }
-
-        //update color for the given coordinate: the average of the newest addition and what it used to be
-        HISTOGRAM_COLOR[cur_y][cur_x][0] = (HISTOGRAM_COLOR[cur_y][cur_x][0] + cur_color[0])/2.0;
-        HISTOGRAM_COLOR[cur_y][cur_x][1] = (HISTOGRAM_COLOR[cur_y][cur_x][1] + cur_color[1])/2.0;
-        HISTOGRAM_COLOR[cur_y][cur_x][2] = (HISTOGRAM_COLOR[cur_y][cur_x][2] + cur_color[2])/2.0;
         
     }
 
@@ -220,10 +223,13 @@ int main()
 
             //Render computed pixel
             G_rgb(avg_color[0],avg_color[1],avg_color[2]);
-            G_point(i+1,j+1);
+            G_point(i/3,j/3);
             
         }
+        printf("Progress: %d/%d blocks rendered \n",i/3,HISTOGRAM_DIM/3);
     }
+
+    printf("FINISHED! \n\n");
 
     int key ;   
     key =  G_wait_key() ; // pause so user can see results
